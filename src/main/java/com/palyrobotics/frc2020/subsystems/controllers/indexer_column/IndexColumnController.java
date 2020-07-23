bf11e01f865c49357d630aedece051e1a559f2e0
aoberai <aoberai05@gmail.com>
2020-07-23 11:44:58 -0700

    added controllers to indexer subsystem

---
+++ src/main/java/com/palyrobotics/frc2020/subsystems/controllers/indexer_column/IndexColumnController.java
@@ -0,0 +1,18 @@
+package com.palyrobotics.frc2020.subsystems.controllers.indexer_column;
+
+import com.palyrobotics.frc2020.robot.RobotState;
+import com.palyrobotics.frc2020.subsystems.Indexer;
+import com.palyrobotics.frc2020.util.control.ControllerOutput;
+
+public class IndexColumnController extends Indexer.IndexerColumnController {
+
+	@Override
+	protected ControllerOutput update(RobotState robotState) {
+		return super.update(robotState);
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return super.isFinished();
+	}
+}
