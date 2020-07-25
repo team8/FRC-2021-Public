27c9d7b63c4b78ced08bbc11bba90942b9931c1c
aoberai <aoberai05@gmail.com>
2020-07-24 17:44:18 -0700

    update

--- src/main/java/com/palyrobotics/frc2020/subsystems/controllers/indexer/IndexColumnController.java
+++ src/main/java/com/palyrobotics/frc2020/subsystems/controllers/indexer/IndexColumnController.java
@@ -1,4 +1,4 @@
-package com.palyrobotics.frc2020.subsystems.controllers.indexer_column;
+package com.palyrobotics.frc2020.subsystems.controllers.indexer;
 
 import com.palyrobotics.frc2020.config.subsystem.IndexerConfig;
 import com.palyrobotics.frc2020.robot.RobotState;
@@ -6,6 +6,7 @@ import com.palyrobotics.frc2020.subsystems.Indexer;
 import com.palyrobotics.frc2020.util.config.Configs;
 import com.palyrobotics.frc2020.util.dashboard.LiveGraph;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpiutil.math.MathUtil;
 
@@ -15,11 +16,12 @@ public class IndexColumnController extends Indexer.IndexerColumnController {
 	private final PIDController mMasterSparkPIDController = new PIDController(mConfig.masterSparkPositionGains.p, mConfig.masterSparkPositionGains.i, mConfig.masterSparkPositionGains.d);
 	private final PIDController mSlaveSparkPIDController = new PIDController(mConfig.slaveSparkPositionGains.p, mConfig.slaveSparkPositionGains.i, mConfig.slaveSparkPositionGains.d);
 	private final double mMasterSparkEncWantedPosition, mSlaveSparkEncWantedPosition;
+	private final Timer mTimer = new Timer();
 
 	public IndexColumnController(RobotState state) {
 		super(state);
-		mMasterSparkEncWantedPosition = state.indexerMasterEncPosition + mConfig.powercellIndexDistance - 4
-		;
+		mTimer.start();
+		mMasterSparkEncWantedPosition = state.indexerMasterEncPosition + mConfig.powercellIndexDistance - 4;
 		mSlaveSparkEncWantedPosition = state.indexerSlaveEncPosition + mConfig.powercellIndexDistance;
 	}
 
@@ -28,15 +30,23 @@ public class IndexColumnController extends Indexer.IndexerColumnController {
 		System.out.println("Running Index Controller");
 		mMasterSparkPIDController.setPID(mConfig.masterSparkPositionGains.p, mConfig.masterSparkPositionGains.i, mConfig.masterSparkPositionGains.d);
 		mSlaveSparkPIDController.setPID(mConfig.slaveSparkPositionGains.p, mConfig.slaveSparkPositionGains.i, mConfig.slaveSparkPositionGains.d);
+
+		mMasterSparkOutput.setPercentOutput(MathUtil.clamp(mMasterSparkPIDController.calculate(state.indexerMasterEncPosition, mMasterSparkEncWantedPosition), -mConfig.maximumIndexerColumnPo, mConfig.maximumIndexerColumnPo));
+		mSlaveSparkOutput.setPercentOutput(MathUtil.clamp(mSlaveSparkPIDController.calculate(state.indexerSlaveEncPosition, mSlaveSparkEncWantedPosition), -mConfig.maximumIndexerColumnPo, mConfig.maximumIndexerColumnPo));
+		if (mTimer.get() > mConfig.indexDuration) {
+			mRightVTalonOutput.setPercentOutput(mConfig.rightVTalonPo);
+			mLeftVTalonOutput.setPercentOutput(mConfig.leftVTalonPo);
+		} else {
+			mRightVTalonOutput.setIdle();
+			mLeftVTalonOutput.setIdle();
+		}
 		LiveGraph.add("MasterTarget", mMasterSparkEncWantedPosition);
 		LiveGraph.add("SlaveTarget", mSlaveSparkEncWantedPosition);
 		LiveGraph.add("isFinished", Math.abs(mMasterSparkEncWantedPosition - state.indexerMasterEncPosition));
-		mMasterSparkOutput.setPercentOutput(MathUtil.clamp(mMasterSparkPIDController.calculate(state.indexerMasterEncPosition, mMasterSparkEncWantedPosition), -0.3, 0.3));
-		mSlaveSparkOutput.setPercentOutput(MathUtil.clamp(mSlaveSparkPIDController.calculate(state.indexerSlaveEncPosition, mSlaveSparkEncWantedPosition), -0.3, 0.3));
 	}
 
 	@Override
 	protected boolean isFinished(RobotState state) {
-		return Math.abs(mMasterSparkEncWantedPosition - state.indexerMasterEncPosition) < mConfig.indexFinishedMinThreshold && Math.abs(mSlaveSparkEncWantedPosition - state.indexerSlaveEncPosition) < mConfig.indexFinishedMinThreshold;
+		return Math.abs(mMasterSparkEncWantedPosition - state.indexerMasterEncPosition) < mConfig.indexFinishedMinThreshold && Math.abs(mSlaveSparkEncWantedPosition - state.indexerSlaveEncPosition) < mConfig.indexFinishedMinThreshold && mTimer.get() > mConfig.indexControllerDuration;
 	}
 }
