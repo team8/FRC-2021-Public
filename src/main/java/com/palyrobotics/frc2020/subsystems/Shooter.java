b5c017e503c6ae555ab5a316ee0a42f5c30ed161
Luca <lm28073@pausd.us>
2020-07-28 11:17:05 -0700

    Created shooter class

---
+++ src/main/java/com/palyrobotics/frc2020/subsystems/Shooter.java
@@ -0,0 +1,12 @@
+package com.palyrobotics.frc2020.subsystems;
+
+import com.palyrobotics.frc2020.robot.Commands;
+import com.palyrobotics.frc2020.robot.RobotState;
+
+public class Shooter extends SubsystemBase {
+
+    @Override
+    public void update(Commands commands, RobotState state) {
+
+    }
+}
