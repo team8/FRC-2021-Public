e6c285e7d3e4ec34ccf1a624698b2e23ed8ce049
Luca <lm28073@pausd.us>
2020-07-28 15:33:43 -0700

    Spotless is bad

--- src/main/java/com/palyrobotics/frc2020/robot/RobotState.java
+++ src/main/java/com/palyrobotics/frc2020/robot/RobotState.java
@@ -2,7 +2,6 @@ package com.palyrobotics.frc2020.robot;
 
 import com.esotericsoftware.minlog.Log;
 import com.palyrobotics.frc2020.config.constants.DriveConstants;
-import com.palyrobotics.frc2020.subsystems.Shooter;
 import com.palyrobotics.frc2020.util.Util;
 
 import edu.wpi.first.wpilibj.geometry.Pose2d;
