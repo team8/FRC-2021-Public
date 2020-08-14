fd70bfad696cfede998dfa6367f72ca2dddbb1d8
Luca <lm28073@pausd.us>
2020-08-14 16:44:46 -0700

    FeedColumnController uses velocity instead of percent output

--- src/main/java/com/palyrobotics/frc2020/subsystems/controllers/indexer/FeedColumnController.java
+++ src/main/java/com/palyrobotics/frc2020/subsystems/controllers/indexer/FeedColumnController.java
@@ -1,13 +1,16 @@
 package com.palyrobotics.frc2020.subsystems.controllers.indexer;
 
 import com.palyrobotics.frc2020.config.constants.IndexerConstants;
+import com.palyrobotics.frc2020.config.subsystem.IndexerConfig;
 import com.palyrobotics.frc2020.robot.RobotState;
 import com.palyrobotics.frc2020.subsystems.Indexer;
+import com.palyrobotics.frc2020.util.config.Configs;
 import com.palyrobotics.frc2020.vision.Limelight;
 
 public class FeedColumnController extends Indexer.IndexerColumnController {
 
 	private Limelight mLimelight = new Limelight();
+	private IndexerConfig mIndexerConfig = Configs.get(IndexerConfig.class);
 
 	public FeedColumnController(RobotState state) {
 		super(state);
@@ -18,8 +21,8 @@ public class FeedColumnController extends Indexer.IndexerColumnController {
 		System.out.println("Running Feed Controller");
 
 		double indexerOutput = IndexerConstants.kTargetDistanceToIndexerVelocity.getInterpolated(mLimelight.getEstimatedDistanceInches());
-		mMasterSparkOutput.setPercentOutput(indexerOutput);
-		mSlaveSparkOutput.setPercentOutput(indexerOutput);
+		mMasterSparkOutput.setTargetVelocity(indexerOutput, mIndexerConfig.masterSparkVelocityGains);
+		mSlaveSparkOutput.setTargetVelocity(indexerOutput, mIndexerConfig.slaveSparkVelocityGains);
 	}
 
 	@Override
