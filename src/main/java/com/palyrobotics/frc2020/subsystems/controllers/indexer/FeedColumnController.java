415d42a6d67b8c7426560f64cfe7631e05d76624
Luca <lm28073@pausd.us>
2020-08-14 16:31:56 -0700

    Reverted robot to no longer contain target distance

--- src/main/java/com/palyrobotics/frc2020/subsystems/controllers/indexer/FeedColumnController.java
+++ src/main/java/com/palyrobotics/frc2020/subsystems/controllers/indexer/FeedColumnController.java
@@ -3,8 +3,11 @@ package com.palyrobotics.frc2020.subsystems.controllers.indexer;
 import com.palyrobotics.frc2020.config.constants.IndexerConstants;
 import com.palyrobotics.frc2020.robot.RobotState;
 import com.palyrobotics.frc2020.subsystems.Indexer;
+import com.palyrobotics.frc2020.vision.Limelight;
 
 public class FeedColumnController extends Indexer.IndexerColumnController {
+
+	private Limelight mLimelight = new Limelight();
 
 	public FeedColumnController(RobotState state) {
 		super(state);
@@ -14,7 +17,7 @@ public class FeedColumnController extends Indexer.IndexerColumnController {
 	protected void update(RobotState state) {
 		System.out.println("Running Feed Controller");
 
-		double indexerOutput = IndexerConstants.kTargetDistanceToIndexerVelocity.getInterpolated(state.targetDistance);
+		double indexerOutput = IndexerConstants.kTargetDistanceToIndexerVelocity.getInterpolated(mLimelight.getEstimatedDistanceInches());
 		mMasterSparkOutput.setPercentOutput(indexerOutput);
 		mSlaveSparkOutput.setPercentOutput(indexerOutput);
 	}
