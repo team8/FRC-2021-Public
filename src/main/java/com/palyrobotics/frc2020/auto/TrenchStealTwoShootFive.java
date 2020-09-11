3953ea75dae69019b62c74754bfe1ac35bfee341
Nolan You <nolanyou35@gmail.com>
2020-09-11 12:47:30 -0700

    secret spotless edit

--- src/main/java/com/palyrobotics/frc2020/auto/TrenchStealTwoShootFive.java
+++ src/main/java/com/palyrobotics/frc2020/auto/TrenchStealTwoShootFive.java
@@ -2,16 +2,13 @@ package com.palyrobotics.frc2020.auto;
 
 import static com.palyrobotics.frc2020.util.Util.newWaypoint;
 
-import com.palyrobotics.frc2020.behavior.ConditionalRoutine;
 import com.palyrobotics.frc2020.behavior.ParallelRaceRoutine;
-import com.palyrobotics.frc2020.behavior.ParallelRoutine;
 import com.palyrobotics.frc2020.behavior.RoutineBase;
 import com.palyrobotics.frc2020.behavior.SequentialRoutine;
-import com.palyrobotics.frc2020.behavior.routines.TimedRoutine;
-import com.palyrobotics.frc2020.behavior.routines.drive.DriveAlignRoutine;
 import com.palyrobotics.frc2020.behavior.routines.drive.DrivePathRoutine;
 import com.palyrobotics.frc2020.behavior.routines.drive.DriveSetOdometryRoutine;
 import com.palyrobotics.frc2020.behavior.routines.drive.DriveYawRoutine;
+
 /**
  * Start by facing and aligning the center of the intake to the middle of the two balls of the
  * opponent's trench. Pull the robot back until just the back bumper covers the initiation line. The
@@ -27,15 +24,11 @@ public class TrenchStealTwoShootFive implements AutoBase {
 				new SequentialRoutine(
 						new DrivePathRoutine(newWaypoint(92, 0, 0))
 								.setMovement(1.5, 2.4),
-						new DriveYawRoutine(15.0)
-				)
-		);
+						new DriveYawRoutine(15.0)));
 //
-		var goToShoot =
-				new DrivePathRoutine(newWaypoint(50, 150, 180))
-						.setMovement(5.0, 8.0)
-						.driveInReverse();
-
+		var goToShoot = new DrivePathRoutine(newWaypoint(50, 150, 180))
+				.setMovement(5.0, 8.0)
+				.driveInReverse();
 
 		return new SequentialRoutine(setInitialOdometry, getTrenchBalls, goToShoot);
 	}
