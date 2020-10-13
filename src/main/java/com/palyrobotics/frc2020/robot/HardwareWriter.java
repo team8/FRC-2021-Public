4fb19114c954eb8a0ae2f0d44f54043ae1f9e5f7
Vedanth P <vedpadu@gmail.com>
2020-10-12 18:34:26 -0700

    fixed the thing where there was an extra mIndexer

--- src/main/java/com/palyrobotics/frc2020/robot/HardwareWriter.java
+++ src/main/java/com/palyrobotics/frc2020/robot/HardwareWriter.java
@@ -46,7 +46,6 @@ public class HardwareWriter {
 	private final Lighting mLighting = Lighting.getInstance();
 	private final Indexer mIndexer = Indexer.getInstance();
 	private final Shooter mShooter = Shooter.getInstance();
-	private final Indexer mIndexer = Indexer.getInstance();
 	private boolean mRumbleOutput;
 
 	void configureHardware(Set<SubsystemBase> enabledSubsystems) {
