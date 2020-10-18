24598657a4e2422ec41b1d418568b58003249a3e
luca <luca1230@gmail.com>
2020-10-18 09:29:26 -0700

    fixed confusing comment

--- src/main/java/com/palyrobotics/frc2020/subsystems/Shooter.java
+++ src/main/java/com/palyrobotics/frc2020/subsystems/Shooter.java
@@ -30,7 +30,7 @@ public class Shooter extends SubsystemBase {
 	private Limelight mLimelight = Limelight.getInstance();
 	private ShooterConfig mConfig = Configs.get(ShooterConfig.class);
 	private boolean mVelocityChanged = false; // If the target velocity has been changed (for rumble)
-	private double mTargetVelocity; // Current target velocity (for rumble)
+	private double mTargetVelocity; // Current target velocity (stored here for rumble)
 	private Timer mTimer = new Timer();
 
 	/* Outputs */
