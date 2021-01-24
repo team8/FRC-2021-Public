2d6e89c306f6bc65a5e9aaac18ddeb95a77b7fae
Nolan You <nolanyou35@gmail.com>
2021-01-24 12:01:40 -0800

    setup first working auto-routine

--- src/main/java/com/palyrobotics/frc2020/auto/GalacticSearchRed.java
+++ src/main/java/com/palyrobotics/frc2020/auto/GalacticSearchRed.java
@@ -4,7 +4,10 @@ import com.palyrobotics.frc2020.behavior.RoutineBase;
 import com.palyrobotics.frc2020.behavior.SequentialRoutine;
 import com.palyrobotics.frc2020.behavior.routines.drive.DrivePathRoutine;
 import com.palyrobotics.frc2020.behavior.routines.drive.DriveSetOdometryRoutine;
+import com.palyrobotics.frc2020.behavior.routines.drive.DriveYawRoutine;
+import com.palyrobotics.frc2020.subsystems.Drive;
 
+import static com.palyrobotics.frc2020.util.Util.newWaypointInches;
 import static com.palyrobotics.frc2020.util.Util.newWaypointMeters;
 
 public class GalacticSearchRed implements AutoBase {
@@ -13,7 +16,19 @@ public class GalacticSearchRed implements AutoBase {
         double xPosInit = 0.76;
         double yPosInit = 2.3;
         var setInitialOdometry = new DriveSetOdometryRoutine(0.76, 2.3, 0);
-        var secondPowercell = new DrivePathRoutine(newWaypointMeters(3.77, 0.77, 30));
-        return new SequentialRoutine(setInitialOdometry, secondPowercell);
+        //will need to split apart for intaking.
+        var path = new DrivePathRoutine(
+                newWaypointMeters(2.286, 2.286, 0),
+                newWaypointMeters(3.81, 1.524, 30),
+                newWaypointMeters(4.572,3.81, 60)
+//                newWaypointMeters(0.3,3.81, -180)
+        );
+
+        var rotate = new DriveYawRoutine(-180);
+
+        var returnHome = new DrivePathRoutine(
+                newWaypointMeters(0.3,3.81, -180)
+        );
+        return new SequentialRoutine(setInitialOdometry, path, rotate, returnHome);
     }
 }
