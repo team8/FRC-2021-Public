c74c36714fa2395d0a3b40e7c0bb51b2bd946eb2
AlanZhang2002 <alanmz2002@gmail.com>
2021-01-24 11:01:36 -0800

    optimized barrelracing a bit

--- src/main/java/com/palyrobotics/frc2020/auto/BarrelRacing.java
+++ src/main/java/com/palyrobotics/frc2020/auto/BarrelRacing.java
@@ -20,10 +20,10 @@ public class BarrelRacing implements AutoBase {
 
 	@Override
 	public RoutineBase getRoutine() {
-		var setInitialOdometry = new DriveSetOdometryRoutine(1.21, 0.662, 20);
-		List<Pose2d> firstCurve = Arrays.asList(newWaypointMeters(2.3, 1.282, 30), newWaypointMeters(2.58, 1.742, 45), newWaypointMeters(4.56, 2.832, 0), newWaypointMeters(5.9, 2.492, -15), newWaypointMeters(6.7, 1.532, -75));
-		List<Pose2d> loop = Arrays.asList(newWaypointMeters(7.18, 0.802, -20), newWaypointMeters(8.4, 0.872, 20), newWaypointMeters(8.75, 1.612, 90), newWaypointMeters(8.32, 2.372, 135), newWaypointMeters(7.27, 2.402, -135), newWaypointMeters(6.7, 1.532, -105));
-		List<Pose2d> secondCurve = Arrays.asList(newWaypointMeters(6.03, 0.722, -165), newWaypointMeters(3.74, 0.622, 170), newWaypointMeters(2.27, 1.552, 135), newWaypointMeters(1.76, 2.172, 160), newWaypointMeters(0.9, 2.442, 180));
+		var setInitialOdometry = new DriveSetOdometryRoutine(1.2, 0.622, 0);
+		List<Pose2d> firstCurve = Arrays.asList(newWaypointMeters(3.2, 2.22, 10), newWaypointMeters(5.86, 2.32, -5));
+		List<Pose2d> loop = Arrays.asList(newWaypointMeters(7.7, 0.74, 0), newWaypointMeters(8.4, 2.052, 120), newWaypointMeters(6.7, 1.532, -105));
+		List<Pose2d> secondCurve = Arrays.asList(newWaypointMeters(5.63, 0.625, -175), newWaypointMeters(3.5, 0.622, 170), newWaypointMeters(1, 2.3, 180));
 		Log.info(kLoggerTag, String.valueOf(Stream.of(firstCurve, loop, secondCurve).flatMap(List::stream).collect(Collectors.toList())));
 		return new SequentialRoutine(setInitialOdometry, new DrivePathRoutine(Stream.of(firstCurve, loop, secondCurve).flatMap(List::stream).collect(Collectors.toList())));
 	}
