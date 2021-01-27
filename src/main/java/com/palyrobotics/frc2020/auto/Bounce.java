853beadab78ac06ee71bedd9eeeafa1c0819bee5
Alexis Kim <ajkim0630@gmail.com>
2021-01-27 11:17:40 -0800

    fixed 180 turn, pt 2

--- src/main/java/com/palyrobotics/frc2020/auto/Bounce.java
+++ src/main/java/com/palyrobotics/frc2020/auto/Bounce.java
@@ -6,20 +6,18 @@ import com.palyrobotics.frc2020.behavior.RoutineBase;
 import com.palyrobotics.frc2020.behavior.SequentialRoutine;
 import com.palyrobotics.frc2020.behavior.routines.drive.DrivePathRoutine;
 import com.palyrobotics.frc2020.behavior.routines.drive.DriveSetOdometryRoutine;
+import com.palyrobotics.frc2020.config.subsystem.DriveConfig;
+import com.palyrobotics.frc2020.util.config.Configs;
 
 public class Bounce implements AutoBase {
 
 	@Override
 	public RoutineBase getRoutine() {
 		var setInitialOdometry = new DriveSetOdometryRoutine(1.2, 2.27, 0);
-		var firstBall = new DrivePathRoutine(newWaypointMeters(2.3, 3.762, 90));
-		var secondBall1 = new DrivePathRoutine(newWaypointMeters(3.6, 1.17, 110)).driveInReverse();
-		var secondBall2 = new DrivePathRoutine(newWaypointMeters(4.5, 1.3, 80));
-		var secondBall3 = new DrivePathRoutine(newWaypointMeters(4.57, 3.762, 90));
-		var thirdBall1 = new DrivePathRoutine(newWaypointMeters(5.0, 1.0, 110)).driveInReverse();
-		var thirdBall2 = new DrivePathRoutine(newWaypointMeters(6.63, 1.45, 60));
-		var thirdBall3 = new DrivePathRoutine(newWaypointMeters(6.85, 3.762, 90));
-		var finishZone = new DrivePathRoutine(newWaypointMeters(8.2, 2.23, 180)).driveInReverse();
-		return new SequentialRoutine(setInitialOdometry, firstBall, secondBall1, secondBall2, secondBall3, thirdBall1, thirdBall2, thirdBall3, finishZone);
+		var firstBall = new DrivePathRoutine(newWaypointMeters(2.3, 3.762, 90)).startingVelocity(Configs.get(DriveConfig.class).pathVelocityMetersPerSecond);
+		var secondBall = new DrivePathRoutine(newWaypointMeters(3.5, 1.0, -180), newWaypointMeters(4.6, 3.762, -90)).driveInReverse().startingVelocity(Configs.get(DriveConfig.class).pathVelocityMetersPerSecond);
+		var thirdBall = new DrivePathRoutine(newWaypointMeters(5.8, 1.0, 0), newWaypointMeters(6.85, 3.672, 90)).startingVelocity(Configs.get(DriveConfig.class).pathVelocityMetersPerSecond);
+		var finishZone = new DrivePathRoutine(newWaypointMeters(8.2, 2.23, 180)).driveInReverse().startingVelocity(Configs.get(DriveConfig.class).pathVelocityMetersPerSecond);
+		return new SequentialRoutine(setInitialOdometry, firstBall, secondBall, thirdBall, finishZone);
 	}
 }
