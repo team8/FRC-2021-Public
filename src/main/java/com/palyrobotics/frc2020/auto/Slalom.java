c1b73187254af942fe5a58fff19a25fd908c1ca3
aoberai <aoberai05@gmail.com>
2021-01-28 11:36:31 -0800

    optimized slalom auto and added acceleration velocity profiles to make faster

--- src/main/java/com/palyrobotics/frc2020/auto/Slalom.java
+++ src/main/java/com/palyrobotics/frc2020/auto/Slalom.java
@@ -24,11 +24,9 @@ public class Slalom implements AutoBase {
 	public RoutineBase getRoutine() {
 		DriveConfig mConfig = Configs.get(DriveConfig.class);
 		var setInitialOdometry = new DriveSetOdometryRoutine(1.46, 2.162, 10);
-		List<Pose2d> firstLoop = Arrays.asList(newWaypointMeters(2.83, 2.412, 10), newWaypointMeters(4.03, 2.132, -30), newWaypointMeters(3.93, 0.942, -180), newWaypointMeters(3.88, 2.04, 10), newWaypointMeters(5.73, 2.312, 15));
-		List<Pose2d> secondLoop = Arrays.asList(newWaypointMeters(6.76, 2.93, 45), newWaypointMeters(6.46, 3.572, 170), newWaypointMeters(5.85, 3.461, -160), newWaypointMeters(5.63, 2.822, -70));
-		List<Pose2d> thirdLoop = Arrays.asList(newWaypointMeters(6.85, 1.23, -20), newWaypointMeters(8.22, 1.522, 90), newWaypointMeters(7.38, 2.252, 180), newWaypointMeters(5.586, 2.262, 160), newWaypointMeters(4.21, 2.702, 180), newWaypointMeters(1.34, 2.662, 180));
-		Log.info(kLoggerTag, String.valueOf(Stream.of(firstLoop, secondLoop).flatMap(List::stream).collect(Collectors.toList())));
-		return new SequentialRoutine(setInitialOdometry, new DrivePathRoutine(Stream.of(firstLoop, secondLoop).flatMap(List::stream).collect(Collectors.toList())), new DrivePathRoutine(thirdLoop).startingVelocity(mConfig.pathVelocityMetersPerSecond));
-//        new DrivePathRoutine(setInitialOdometry, firstLoop, secondLoop);
+		List<Pose2d> firstLoop = Arrays.asList(newWaypointMeters(2.83, 2.412, 10), newWaypointMeters(4.03, 2.132, -30), newWaypointMeters(3.93, 0.7, -180), newWaypointMeters(3.88, 2.04, 10), newWaypointMeters(5.73, 2.312, 15));
+		List<Pose2d> secondLoop = Arrays.asList(newWaypointMeters(6.89, 3.342, 100), newWaypointMeters(6.15, 3.862, -170), newWaypointMeters(5.57, 3.032, -70));
+		List<Pose2d> thirdLoop = Arrays.asList(newWaypointMeters(6.85, 1.23, -20), newWaypointMeters(8.22, 1.522, 90), newWaypointMeters(7.38, 2.252, 180), newWaypointMeters(5.586, 2.262, 180), newWaypointMeters(0.5, 2.662, 180));
+		return new SequentialRoutine(setInitialOdometry, new DrivePathRoutine(firstLoop).endingVelocity(mConfig.pathVelocityMetersPerSecond), new DrivePathRoutine(secondLoop).endingVelocity(mConfig.pathVelocityMetersPerSecond).startingVelocity(mConfig.pathVelocityMetersPerSecond), new DrivePathRoutine(thirdLoop).setMovement(mConfig.pathVelocityMetersPerSecond * 1.4, mConfig.pathAccelerationMetersPerSecondSquared*1.3).startingVelocity(mConfig.pathVelocityMetersPerSecond));
 	}
 }
