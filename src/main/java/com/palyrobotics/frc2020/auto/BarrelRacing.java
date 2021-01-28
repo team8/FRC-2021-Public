81f57976cd3650c4f886529af5f73fd806d476b6
AlanZhang2002 <alanmz2002@gmail.com>
2021-01-28 12:51:56 -0800

    robot goes nyoom, still needs testing

--- src/main/java/com/palyrobotics/frc2020/auto/BarrelRacing.java
+++ src/main/java/com/palyrobotics/frc2020/auto/BarrelRacing.java
@@ -27,22 +27,17 @@ public class BarrelRacing implements AutoBase {
 		// Making one large routine since there shouldn't be any stops along the way, just one large motion
 		var path = new DrivePathRoutine(
 				/** -=-First Curve-=- **/
-				newWaypointMeters(2.6, 2, 25),
+				newWaypointMeters(2.6, 1.85, 20),
 				newWaypointMeters(5.9, 2.08, -15),
 
 				/** -=-=-=-Loop-=-=-=- **/
-				newWaypointMeters(7.4, 1.1, 0),
-				newWaypointMeters(7.85, 1.8, 140),
-				newWaypointMeters(6.98, 1.48, -105),
-				/* Wide loop, slower */
-//				newWaypointMeters(7.4, 0.78, 0),
-//				newWaypointMeters(8.2, 1.99, 140),
-//				newWaypointMeters(6.95, 1.55, -105),
+				newWaypointMeters(7.44, 1.1, -5),
+				newWaypointMeters(7.80, 1.8, 140),
 
 				/** -=-Second Curve-=- **/
-				newWaypointMeters(5.75, 1.072, -175),
-				newWaypointMeters(3.04, 1.11, 175),
-				newWaypointMeters(1.25, 1.98, 175))
+				newWaypointMeters(6.66, 1.432, -155),
+				newWaypointMeters(3.04, 1.07, 160),
+				newWaypointMeters(1.25, 1.93, 175))
 						.setMovement(5.3, 3.2);
 
 		return new SequentialRoutine(setInitialOdometry, path);
