5c37b348f16928dbc0cb0112037e21ef101b429c
Nolan You <nolanyou35@gmail.com>
2021-02-04 15:49:55 -0800

    edited GalacticSearchABlue

--- src/main/java/com/palyrobotics/frc2020/auto/GalacticSearchABlue.java
+++ src/main/java/com/palyrobotics/frc2020/auto/GalacticSearchABlue.java
@@ -5,12 +5,19 @@ import static com.palyrobotics.frc2020.util.Util.newWaypointMeters;
 import com.palyrobotics.frc2020.behavior.ParallelRoutine;
 import com.palyrobotics.frc2020.behavior.RoutineBase;
 import com.palyrobotics.frc2020.behavior.SequentialRoutine;
+import com.palyrobotics.frc2020.behavior.routines.drive.DriveParallelPathRoutine;
 import com.palyrobotics.frc2020.behavior.routines.drive.DrivePathRoutine;
 import com.palyrobotics.frc2020.behavior.routines.drive.DriveSetOdometryRoutine;
 import com.palyrobotics.frc2020.behavior.routines.superstructure.IntakeBallRoutine;
 import com.palyrobotics.frc2020.behavior.routines.superstructure.IntakeLowerRoutine;
+import com.palyrobotics.frc2020.behavior.routines.superstructure.IntakeStowRoutine;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.util.Units;
+
+import java.util.function.Predicate;
 
 public class GalacticSearchABlue implements AutoBase {
+	Predicate<Pose2d> nearFirstBall = poseMeters -> poseMeters.getTranslation().getX() > Units.inchesToMeters(70.0);
 
 	@Override
 	public RoutineBase getRoutine() {
@@ -22,9 +29,10 @@ public class GalacticSearchABlue implements AutoBase {
 				newWaypointMeters(4.572, 0.76, 0),
 				newWaypointMeters(5.334, 3.048, 60),
 				newWaypointMeters(6.858, 2.286, -60));
-		var pathAndIntake = new ParallelRoutine(path, new SequentialRoutine(new IntakeLowerRoutine(), new IntakeBallRoutine(6.0)));
-		var returnHome = new DrivePathRoutine(
-				newWaypointMeters(8.763, 2.286, 0));
+		var returnHome = new ParallelRoutine(new DrivePathRoutine(
+				newWaypointMeters(8.763, 2.286, 0)), new IntakeStowRoutine());
+		var pathAndIntake = new DriveParallelPathRoutine(path, new SequentialRoutine(new IntakeLowerRoutine(), new IntakeBallRoutine(6.0)), nearFirstBall);
+
 		return new SequentialRoutine(setInitialOdometry, pathAndIntake, returnHome);
 	}
 }
