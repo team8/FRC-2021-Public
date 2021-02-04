8b42a0184b8d330b40743df9f90be1b802c0d62b
Nolan You <nolanyou35@gmail.com>
2021-02-04 15:57:45 -0800

    added galacticSearchBBlue changes. May need further tweaking.

--- src/main/java/com/palyrobotics/frc2020/auto/GalacticSearchBBlue.java
+++ src/main/java/com/palyrobotics/frc2020/auto/GalacticSearchBBlue.java
@@ -9,18 +9,19 @@ import com.palyrobotics.frc2020.behavior.routines.drive.DrivePathRoutine;
 import com.palyrobotics.frc2020.behavior.routines.drive.DriveSetOdometryRoutine;
 import com.palyrobotics.frc2020.behavior.routines.superstructure.IntakeBallRoutine;
 import com.palyrobotics.frc2020.behavior.routines.superstructure.IntakeLowerRoutine;
+import com.palyrobotics.frc2020.behavior.routines.superstructure.IntakeStowRoutine;
 
 public class GalacticSearchBBlue implements AutoBase {
 
 	@Override
 	public RoutineBase getRoutine() {
 		var setInitialOdometry = new DriveSetOdometryRoutine(0.5, 2.3, 0);
-		var firstBall = new DrivePathRoutine(newWaypointMeters(4.5, 1.5, 30));
-		var secondBall = new DrivePathRoutine(newWaypointMeters(6.09, 3.07, 0));
-		var thirdBall = new DrivePathRoutine(newWaypointMeters(7.8, 1.5, 0));
-		var finishZone = new DrivePathRoutine(newWaypointMeters(8.75, 2.23, 0));
-		var path = new SequentialRoutine(firstBall, secondBall, thirdBall);
-		var pathAndIntake = new ParallelRoutine(path, new SequentialRoutine(new IntakeLowerRoutine(), new IntakeBallRoutine(6.0)));
+		var balls = new DrivePathRoutine(
+				newWaypointMeters(4.5, 1.5, 30),
+				newWaypointMeters(6.09, 3.07, 0),
+				newWaypointMeters(7.8, 1.5, 0));
+		var finishZone = new ParallelRoutine(new DrivePathRoutine(newWaypointMeters(8.75, 2.23, 0)), new IntakeStowRoutine());
+		var pathAndIntake = new ParallelRoutine(balls, new SequentialRoutine(new IntakeLowerRoutine(), new IntakeBallRoutine(6.0)));
 
 		return new SequentialRoutine(setInitialOdometry, pathAndIntake, finishZone);
 	}
