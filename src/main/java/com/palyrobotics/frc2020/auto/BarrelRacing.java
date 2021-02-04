596b91b2e9f0f7d1e2d3ca80b758778434d6d982
AlanZhang2002 <alanmz2002@gmail.com>
2021-02-04 00:01:08 -0800

    recreated points based on actual robot dimensions, needs testing

--- src/main/java/com/palyrobotics/frc2020/auto/BarrelRacing.java
+++ src/main/java/com/palyrobotics/frc2020/auto/BarrelRacing.java
@@ -11,29 +11,32 @@ public class BarrelRacing implements AutoBase {
 
 	@Override
 	public RoutineBase getRoutine() {
-		var setInitialOdometry = new DriveSetOdometryRoutine(1.23, 1.05, 0);
+		var setInitialOdometry = new DriveSetOdometryRoutine(1.0, 0.8, 0);
 		// Making one large routine since there shouldn't be any stops along the way, just one large motion
 		var path = new DrivePathRoutine(
 				/** -=-First Curve-=- **/
-				newWaypointMeters(2.6, 1.85, 20),
+				newWaypointMeters(2.8, 2.1, 35),
 //				newWaypointMeters(5.9, 1.85, -15),
-				newWaypointMeters(6.1, 1.93, -35),
+//				newWaypointMeters(6.25, 2.3, -50),
+				newWaypointMeters(6.5, 1.9, -50),
 
 				/** -=-=-=-Loop-=-=-=- **/
-				newWaypointMeters(7.30, 1.20, -30),
-//				newWaypointMeters(7.6, 1.05, 0),
-				newWaypointMeters(8.17, 1.55, 90),
-				newWaypointMeters(7.58, 2.0, -170),
-
-//				newWaypointMeters(7.44, 1.1, -5),
-//				newWaypointMeters(7.80, 1.8, 140),
+//				newWaypointMeters(7.07, 0.85, -35),
+//				newWaypointMeters(7.30, 0.73, -20),
+//				newWaypointMeters(7.58, 0.69, 0),
+				newWaypointMeters(8.09, 0.73, 10),
+//				newWaypointMeters(8.57, 1.05, 75),
+//				newWaypointMeters(8.36, 0.62, 20),
+//				newWaypointMeters(8.39, 2.27, 135),
+				newWaypointMeters(8.18, 2.43, 160),
+//				newWaypointMeters(7.02, 2.12, -120),
+				newWaypointMeters(6.75, 1.66, -110),
 
 				/** -=-Second Curve-=- **/
-				newWaypointMeters(6.4, 1.12, -160),
-
-//				newWaypointMeters(6.66, 1.432, -155),
-				newWaypointMeters(3.04, 1.07, 160),
-				newWaypointMeters(1.25, 1.93, 175));
+//				newWaypointMeters(6.3, 0.8, -160),
+				newWaypointMeters(5.66, 0.62, -165),
+				newWaypointMeters(2.83, 0.87, 138),
+				newWaypointMeters(1.25, 2.15, 180));
 
 		return new SequentialRoutine(setInitialOdometry, path);
 	}
