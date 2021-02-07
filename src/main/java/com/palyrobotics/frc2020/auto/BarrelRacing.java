6ba862f586e14c949a1cac7545c086ffe1b975a4
AlanZhang2002 <alanmz2002@gmail.com>
2021-02-06 19:09:20 -0800

    switched names of Slalom and BarrelRacing to match actual names (whoops :p)

--- src/main/java/com/palyrobotics/frc2020/auto/BarrelRacing.java
+++ src/main/java/com/palyrobotics/frc2020/auto/BarrelRacing.java
@@ -6,35 +6,32 @@ import com.palyrobotics.frc2020.behavior.RoutineBase;
 import com.palyrobotics.frc2020.behavior.SequentialRoutine;
 import com.palyrobotics.frc2020.behavior.routines.drive.DrivePathRoutine;
 import com.palyrobotics.frc2020.behavior.routines.drive.DriveSetOdometryRoutine;
+import com.palyrobotics.frc2020.config.subsystem.DriveConfig;
+import com.palyrobotics.frc2020.util.config.Configs;
 
 public class BarrelRacing implements AutoBase {
 
 	@Override
 	public RoutineBase getRoutine() {
-		var setInitialOdometry = new DriveSetOdometryRoutine(1.23, 1.05, 0);
-		// Making one large routine since there shouldn't be any stops along the way, just one large motion
+		DriveConfig mConfig = Configs.get(DriveConfig.class);
+//		var setInitialOdometry = new DriveSetOdometryRoutine(1.46, 2.162, 10);
+//		List<Pose2d> firstLoop = Arrays.asList(newWaypointMeters(2.83, 2.412, 10), newWaypointMeters(4.03, 2.132, -30), newWaypointMeters(3.93, 0.7, -180), newWaypointMeters(3.88, 2.04, 10), newWaypointMeters(5.73, 2.312, 15));
+//		List<Pose2d> secondLoop = Arrays.asList(newWaypointMeters(6.89, 3.342, 100), newWaypointMeters(6.15, 3.862, -170), newWaypointMeters(5.57, 3.032, -70));
+//		List<Pose2d> thirdLoop = Arrays.asList(newWaypointMeters(6.85, 1.23, -20), newWaypointMeters(8.22, 1.522, 90), newWaypointMeters(7.38, 2.252, 180), newWaypointMeters(5.586, 2.262, 180), newWaypointMeters(0.5, 2.662, 180));
+//		return new SequentialRoutine(setInitialOdometry, new DrivePathRoutine(firstLoop).endingVelocity(mConfig.pathVelocityMetersPerSecond), new DrivePathRoutine(secondLoop).endingVelocity(mConfig.pathVelocityMetersPerSecond).startingVelocity(mConfig.pathVelocityMetersPerSecond), new DrivePathRoutine(thirdLoop).setMovement(mConfig.pathVelocityMetersPerSecond * 1.4, mConfig.pathAccelerationMetersPerSecondSquared * 1.3).startingVelocity(mConfig.pathVelocityMetersPerSecond));
+
+		var setInitialOdometry = new DriveSetOdometryRoutine(1.18, 2.672, -0);
 		var path = new DrivePathRoutine(
-				/** -=-First Curve-=- **/
-				newWaypointMeters(2.6, 1.85, 20),
-//				newWaypointMeters(5.9, 1.85, -15),
-				newWaypointMeters(6.1, 1.93, -35),
-
-				/** -=-=-=-Loop-=-=-=- **/
-				newWaypointMeters(7.30, 1.20, -30),
-//				newWaypointMeters(7.6, 1.05, 0),
-				newWaypointMeters(8.17, 1.55, 90),
-				newWaypointMeters(7.58, 2.0, -170),
-
-//				newWaypointMeters(7.44, 1.1, -5),
-//				newWaypointMeters(7.80, 1.8, 140),
-
-				/** -=-Second Curve-=- **/
-				newWaypointMeters(6.4, 1.12, -160),
-
-//				newWaypointMeters(6.66, 1.432, -155),
-				newWaypointMeters(3.04, 1.07, 160),
-				newWaypointMeters(1.25, 1.93, 175));
-
+				/** First loop **/
+				newWaypointMeters(3.63, 2.03, -15),
+				newWaypointMeters(3.72, 1.132, 170),
+				newWaypointMeters(3.89, 2.062, 15),
+				/** Second loop **/
+				newWaypointMeters(6.21, 2.72, 50),
+				newWaypointMeters(5.79, 3.36, -140),
+				/** Third loop **/
+				newWaypointMeters(7.88, 1.322, 40),
+				newWaypointMeters(1.2, 2.2, 180));
 		return new SequentialRoutine(setInitialOdometry, path);
 	}
 }
