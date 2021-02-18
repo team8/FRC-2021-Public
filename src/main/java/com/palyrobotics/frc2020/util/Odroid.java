12e4389248166c818dd2e2a9a7162fe00d1e2b5a
luca <luca1230@gmail.com>
2021-02-17 16:57:41 -0800

    changed the way messages are stored

--- src/main/java/com/palyrobotics/frc2020/util/Odroid.java
+++ src/main/java/com/palyrobotics/frc2020/util/Odroid.java
@@ -2,6 +2,7 @@ package com.palyrobotics.frc2020.util;
 
 import java.io.IOException;
 
+import com.esotericsoftware.kryonet.Client;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
@@ -13,15 +14,10 @@ public class Odroid {
 	private static final String catagory = "Odroid";
 
 	private Server server;
-	private String address;
-	private int port;
 
-	private double randiansToBall = 0;
+	private double radiansToBall = 0;
 
 	public Odroid(String address, int port) {
-		this.address = address;
-		this.port = port;
-
 		server = new Server();
 		server.getKryo().setRegistrationRequired(false);
 
@@ -46,15 +42,12 @@ public class Odroid {
 
 					// it would be easier just to send a double, but i feel like this is better in case
 					// that more stuff needs to be transmitted
-					if (((OdroidMessage) message).type == 0) {
-						if (((OdroidMessage) message).message.getClass().equals(Double.class)) {
-							randiansToBall = (Double) ((OdroidMessage) message).message;
-						}
-					}
+                    radiansToBall = ((OdroidMessage) message).radiansToBall;
 				}
 			});
 			server.start();
 			server.bind(port);
+
 			Log.info(catagory, "Started server");
 		} catch (IOException exception) {
 			Log.error(catagory, "Failed to start server", exception);
@@ -63,6 +56,6 @@ public class Odroid {
 
 	// might want to go to degrees
 	public double getRadiansToBall() {
-		return randiansToBall;
+		return radiansToBall;
 	}
 }
--- src/main/java/com/palyrobotics/frc2020/util/service/OdroidMessage.java
+++ src/main/java/com/palyrobotics/frc2020/util/service/OdroidMessage.java
@@ -1,7 +1,5 @@
 package com.palyrobotics.frc2020.util.service;
 
 public class OdroidMessage {
-
-	public int type;
-	public Object message;
+    public double radiansToBall;
 }
