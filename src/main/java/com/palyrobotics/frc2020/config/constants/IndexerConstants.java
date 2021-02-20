0734b0f22271a980a0dbc891610ded6ba126fc7f
aoberai <aoberai05@gmail.com>
2021-02-20 13:57:35 -0800

    fixed indexer feed all issues and added decent indexer velocity gains

--- src/main/java/com/palyrobotics/frc2020/config/constants/IndexerConstants.java
+++ src/main/java/com/palyrobotics/frc2020/config/constants/IndexerConstants.java
@@ -7,7 +7,7 @@ public class IndexerConstants {
 	public static final InterpolatingDoubleTreeMap kTargetDistanceToIndexerVelocity = new InterpolatingDoubleTreeMap();
 
 	static {
-		kTargetDistanceToIndexerVelocity.put(0.0, 0.0); // Need robot to add value, this is how to do them
+		kTargetDistanceToIndexerVelocity.put(0.0, 80.0);
 	}
 
 }
