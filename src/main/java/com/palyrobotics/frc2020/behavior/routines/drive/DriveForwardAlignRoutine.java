9d9011aa6d3b0f73da97703bfb22f2214a49e8cf
Nolan You <nolanyou35@gmail.com>
2021-03-17 16:39:31 -0700

    need to figure out accuracy issue/ how to find straight error

--- src/main/java/com/palyrobotics/frc2020/behavior/routines/drive/DriveForwardAlignRoutine.java
+++ src/main/java/com/palyrobotics/frc2020/behavior/routines/drive/DriveForwardAlignRoutine.java
@@ -5,6 +5,7 @@ import static com.palyrobotics.frc2020.util.Util.getDifferenceInAngleDegrees;
 import java.util.Objects;
 import java.util.Set;
 
+import com.palyrobotics.frc2020.behavior.RoutineBase;
 import com.palyrobotics.frc2020.config.VisionConfig;
 import com.palyrobotics.frc2020.robot.Commands;
 import com.palyrobotics.frc2020.robot.ReadOnly;
@@ -12,6 +13,7 @@ import com.palyrobotics.frc2020.robot.RobotState;
 import com.palyrobotics.frc2020.subsystems.SubsystemBase;
 import com.palyrobotics.frc2020.util.config.Configs;
 import com.palyrobotics.frc2020.vision.Limelight;
+import edu.wpi.first.vision.VisionPipeline;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 
 public class DriveForwardAlignRoutine extends DrivePathRoutine {
@@ -19,11 +21,16 @@ public class DriveForwardAlignRoutine extends DrivePathRoutine {
     private final VisionConfig mVisionConfig = Configs.get(VisionConfig.class);
     private final Limelight mLimelight = Limelight.getInstance();
     private final int mVisionPipeline;
-    private Pose2d mTargetPos;
+    //Limelight controlled targeted point.
+    private Pose2d mLimelightTarget;
+
     private double mTargetYawDegrees;
     private double mTrajectory;
 
-    public DriveForwardAlignRoutine(int visionPipeline, double straightDistanceMeters) {
+    private boolean hasLocated;
+
+    public DriveForwardAlignRoutine(Pose2d forwardTarget, int visionPipeline) {
+        super(forwardTarget);
         mVisionPipeline = visionPipeline;
     }
 
@@ -32,18 +39,20 @@ public class DriveForwardAlignRoutine extends DrivePathRoutine {
         //check if target, then if target is there create a trajectory once that is the current pos to target pos.
         commands.visionWanted = true;
         if (mLimelight.isTargetFound()) {
+            hasLocated = true;
             mTargetYawDegrees = mLimelight.getYawToTarget();
-            commands.setDriveVisionAlign(mVisionPipeline);
+           //TODO: fill this out mLimelightTarget = new Pose2d();
+            commands.addWantedRoutine(new DrivePathRoutine());
+        }
+        else{
+            super.update(commands, state);
         }
     }
 
-
     @Override
     public boolean checkIfFinishedEarly(@ReadOnly RobotState state) {
-        //TODO: need to implement
-        return false;
+        return hasLocated;
     }
-
     @Override
     public Set<SubsystemBase> getRequiredSubsystems() {
         return Set.of(mDrive);
